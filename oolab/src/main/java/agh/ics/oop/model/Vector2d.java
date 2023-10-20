ea5e68c3df04787e70d1dc44b36f5f50d5c1c10d
LAPTOP-N9N63R0J\Kacper_Legion <kbieniasz@student.agh.edu.pl>
2023-10-20 20:43:26 +0200

    change test and equals in vector

--- oolab/src/main/java/agh/ics/oop/model/Vector2d.java
+++ oolab/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -1,5 +1,7 @@
 package agh.ics.oop.model;
 
+import java.util.Objects;
+
 public class Vector2d {
 
     private final int x;
@@ -19,38 +21,50 @@ public class Vector2d {
         return this.y;
     }
 
+    @Override
     public String toString() {
         return "(%d,%d)".formatted(this.x, this.y);
     }
 
-    public boolean precedes(Vector2d other){
+    public boolean precedes(Vector2d other) {
         return (this.x <= other.x) && (this.y <= other.y);
     }
-    public boolean follows(Vector2d other){
+
+    public boolean follows(Vector2d other) {
         return this.x >= other.x && this.y >= other.y;
     }
-    public Vector2d add(Vector2d other){
-        return new Vector2d(this.x+other.x,this.y+other.y);
-    }
-    public Vector2d substract(Vector2d other){
-        return new Vector2d(this.x-other.x,this.y-other.y);
-    }
-    public Vector2d upperRight(Vector2d other){
-        return new Vector2d(Math.max(this.x, other.x), Math.max(this.y,other.y));
+
+    public Vector2d add(Vector2d other) {
+        return new Vector2d(this.x + other.x, this.y + other.y);
     }
 
-    public Vector2d lowerLeft(Vector2d other){
-        return new Vector2d(Math.min(this.x, other.x),Math.min(this.y, other.y));
-    }
-    public Vector2d opposite(){
-        return new Vector2d(-this.x,-this.y);
+    public Vector2d subtract(Vector2d other) {
+        return new Vector2d(this.x - other.x, this.y - other.y);
     }
 
+    public Vector2d upperRight(Vector2d other) {
+        return new Vector2d(Math.max(this.x, other.x), Math.max(this.y, other.y));
+    }
+
+    public Vector2d lowerLeft(Vector2d other) {
+        return new Vector2d(Math.min(this.x, other.x), Math.min(this.y, other.y));
+    }
+
+    public Vector2d opposite() {
+        return new Vector2d(-this.x, -this.y);
+    }
+
+    @Override
     public boolean equals(Object other){
         if(this == other) return true;
         if(!(other instanceof Vector2d))
             return false;
         Vector2d that = (Vector2d) other;
-        return that.x==this.x && that.y == this.x;
+        return (that.x==this.x && that.y == this.y);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(x,y);
     }
 }
