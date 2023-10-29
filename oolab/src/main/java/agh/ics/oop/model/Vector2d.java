0344db2ca83c8f5410e88e582a7a0b5d9037e844
Kacper Bieniasz <kbieniasz@student.agh.edu.pl>
2023-10-29 14:12:29 +0100

    minor look changes

--- oolab/src/main/java/agh/ics/oop/model/Vector2d.java
+++ oolab/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -14,44 +14,44 @@ public class Vector2d {
     }
 
     public int getX() {
-        return this.x;
+        return x;
     }
 
     public int getY() {
-        return this.y;
+        return y;
     }
 
     @Override
     public String toString() {
-        return "(%d,%d)".formatted(this.x, this.y);
+        return "(%d,%d)".formatted(x, y);
     }
 
     public boolean precedes(Vector2d other) {
-        return (this.x <= other.x) && (this.y <= other.y);
+        return x <= other.x && y <= other.y;
     }
 
     public boolean follows(Vector2d other) {
-        return this.x >= other.x && this.y >= other.y;
+        return x >= other.x && y >= other.y;
     }
 
     public Vector2d add(Vector2d other) {
-        return new Vector2d(this.x + other.x, this.y + other.y);
+        return new Vector2d(x + other.x, y + other.y);
     }
 
     public Vector2d subtract(Vector2d other) {
-        return new Vector2d(this.x - other.x, this.y - other.y);
+        return new Vector2d(x - other.x, y - other.y);
     }
 
     public Vector2d upperRight(Vector2d other) {
-        return new Vector2d(Math.max(this.x, other.x), Math.max(this.y, other.y));
+        return new Vector2d(Math.max(x, other.x), Math.max(y, other.y));
     }
 
     public Vector2d lowerLeft(Vector2d other) {
-        return new Vector2d(Math.min(this.x, other.x), Math.min(this.y, other.y));
+        return new Vector2d(Math.min(x, other.x), Math.min(y, other.y));
     }
 
     public Vector2d opposite() {
-        return new Vector2d(-this.x, -this.y);
+        return new Vector2d(-x, -y);
     }
 
     @Override
@@ -60,7 +60,7 @@ public class Vector2d {
         if(!(other instanceof Vector2d))
             return false;
         Vector2d that = (Vector2d) other;
-        return (that.x==this.x && that.y == this.y);
+        return (that.x==x && that.y == y);
     }
 
     @Override
