8a9a449835406be39b98de884bd2fce5da51feba
Kacper Bieniasz <kbieniasz@student.agh.edu.pl>
2023-10-29 14:11:50 +0100

    add code in main to verify simulation

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,11 +1,11 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.MapDirection;
 import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Vector2d;
+import java.util.List;
 
 public class World {
-    public static void run(MoveDirection[] directions){
+    public static void run(List<MoveDirection> directions){
         for(MoveDirection direction:directions){
             switch(direction){
                 case FORWARD -> System.out.println("Zwierzak idzie do przodu");
@@ -16,18 +16,9 @@ public class World {
         }
     }
     public static void main(String[] args){
-        System.out.println("System wystartował");
-        MoveDirection[] moves = OptionsParser.parse(args);
-        run(moves);
-        System.out.println("System zakończył działanie");
-        Vector2d position1 = new Vector2d(1,2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2,1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
-        System.out.println(MapDirection.SOUTH.next());
-        System.out.println(MapDirection.SOUTH.previous());
-        Vector2d position3 = MapDirection.SOUTH.toUnitVector();
-        System.out.println(position3);
+        List<MoveDirection> directions = OptionsParser.parse(args);
+        List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
+        Simulation simulation = new Simulation(directions, positions);
+        simulation.run();
     }
 }
