4135ffeadd76521adad64bdef36b0f8133458591
Kacper Bieniasz <kbieniasz@student.agh.edu.pl>
2023-10-30 16:35:35 +0100

    OptionParserTest using ArrayList

--- oolab/src/test/java/agh/ics/oop/model/OptionParserTest.java
+++ oolab/src/test/java/agh/ics/oop/model/OptionParserTest.java
@@ -2,6 +2,8 @@ package agh.ics.oop.model;
 
 import agh.ics.oop.OptionsParser;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class OptionParserTest {
@@ -10,7 +12,7 @@ public class OptionParserTest {
         String[] args1 = {"f", "b", "l", "r"};
         String[] args2 = {"f", "b", "asd", "r"};
 
-        assertArrayEquals(new MoveDirection[]{MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.LEFT, MoveDirection.RIGHT}, OptionsParser.parse(args1));
-        assertArrayEquals(new MoveDirection[]{MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.RIGHT}, OptionsParser.parse(args2));
+        assertEquals(new ArrayList<MoveDirection>(List.of(new MoveDirection[]{MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.LEFT, MoveDirection.RIGHT})), OptionsParser.parse(args1));
+        assertEquals(new ArrayList<MoveDirection>(List.of(new MoveDirection[]{MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.RIGHT})), OptionsParser.parse(args2));
     }
 }
